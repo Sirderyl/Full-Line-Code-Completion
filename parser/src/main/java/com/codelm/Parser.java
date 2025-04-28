834a5a23fc14bbdf071ce7f5460824b59e791576
Sirderyl <93952244+Sirderyl@users.noreply.github.com>
2025-04-28 02:51:11 +0200

    Removing metadata from Java files

--- parser/src/main/java/com/codelm/Parser.java
+++ parser/src/main/java/com/codelm/Parser.java
@@ -7,12 +7,21 @@ import com.github.javaparser.ast.comments.Comment;
 import com.github.javaparser.ast.nodeTypes.NodeWithAnnotations;
 import com.github.javaparser.printer.DefaultPrettyPrinter;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Parser {
+
+    private static final Pattern JAVA_CODE_START = Pattern.compile(
+            "^\\s*(package|import|public|class|interface|enum|@interface).*"
+    );
 
     public Parser() {}
 
     public String cleanJavaCode(String javaCode) {
-        CompilationUnit cu = StaticJavaParser.parse(javaCode);
+        String noPrefixCode = removePrefixLines(javaCode);
+
+        CompilationUnit cu = StaticJavaParser.parse(noPrefixCode);
 
         // Remove comments
         cu.getAllComments().forEach(Comment::remove);
@@ -29,6 +38,18 @@ public class Parser {
         //cu.findAll(AnnotationMemberDeclaration.class).forEach(Node::remove);
 
         return cu.toString().replaceAll("\\s+", " ");
+    }
+
+    private String removePrefixLines(String javaCode) {
+        String[] lines = javaCode.split("\n");
+        int startIndex = 0;
+        for (String line : lines) {
+            if (JAVA_CODE_START.matcher(line).matches()) {
+                break;
+            }
+            startIndex++;
+        }
+        return String.join("\n", Arrays.copyOfRange(lines, startIndex, lines.length));
     }
 
     public String formatJavaCode(String cleanCode){
